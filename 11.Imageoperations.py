8f03322e68486fb73e3398316af26b63ef2041ae
NITHIN V S <110377337+codexcherry@users.noreply.github.com>
2025-07-01 20:20:43 +0530

    Update 11.Imageoperations.py

--- 11.Imageoperations.py
+++ 11.Imageoperations.py
@@ -1,27 +1,19 @@
-import cv2
-import numpy as np
-import matplotlib.pyplot as plt
+import cv2,numpy as np,matplotlib.pyplot as plt
 
-image = cv2.imread('image.jpeg')
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+img = cv2.imread('One-Piece-Anime-PNG-High-Quality-Image.png')
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
-# Apply binary thresholding
-_, binary = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY_INV)
-
-# Find connected components
-num_labels, labels_im = cv2.connectedComponents(binary)
-
-# Generate a color image for labels
-output = np.zeros_like(image)
-
-for i in range(1, num_labels):
-    output[labels_im == i] = colors[i]
-
-plt.imshow(image)
-plt.show()
-
+_, binary = cv2.threshold(gray,128,255,cv2.THRESH_BINARY_INV)
 plt.imshow(binary)
 plt.show()
+
+no_l , l_i = cv2.connectedComponents(binary)
+
+output = np.zeros_like(img)
+colors = np.random.randint(0, 255, size=(no_l, 3))
+
+for i in range(1,no_l):
+    output[l_i == i] = colors[i]
 
 plt.imshow(output)
 plt.show()
